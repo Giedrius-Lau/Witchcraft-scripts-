e0c60c37067ea15ee4610ddc831fa093bf03fd54
Giedrius <lauruska.giedrius@gmail.com>
2021-02-17 10:22:09 +0200

    save azeta

--- css/Aazeta.lv.js
+++ css/Aazeta.lv.js
@@ -11,46 +11,8 @@ setInterval(function () {
 
         var trendingSearches = document.createElement('div');
         trendingSearches.setAttribute('class', 'test-container');
-        trendingSearches.innerHTML = `
-        <div class="first-block">
-            <div class="block-title"><p>Meklēšanas vēsture</p></div>
-            <div class="test-history-list"></div>
-        </div>
-        <div class="second-block">
-        <div class="block-title"><img src="https://semexit.s3-eu-west-1.amazonaws.com/Azeta/star.svg" alt="star"><p>Aktuālie meklējumi</p></div>
-            <div class="suggestions-list">
-                <div class="suggestion-link">
-                    <a href="/meklet/rezultati?q=bioderma">Bioderma
-                        <img src="https://semexit.s3-eu-west-1.amazonaws.com/Azeta/long-arrow-up.svg" alt="arrow">
-                    </a>
-                </div>
-                <div class="suggestion-link">
-                    <a href="/meklet/rezultati?q=Vitamīns D">Vitamīns D
-                        <img src="https://semexit.s3-eu-west-1.amazonaws.com/Azeta/long-arrow-up.svg" alt="arrow">
-                    </a>
-                </div>
-                <div class="suggestion-link">
-                    <a href="/meklet/rezultati?q=Magnijs">Magnijs
-                        <img src="https://semexit.s3-eu-west-1.amazonaws.com/Azeta/long-arrow-up.svg" alt="arrow">
-                    </a>
-                </div>
-                <div class="suggestion-link">
-                    <a href="/meklet/rezultati?q=Eucerin">Eucerin
-                        <img src="https://semexit.s3-eu-west-1.amazonaws.com/Azeta/long-arrow-up.svg" alt="arrow">
-                    </a>
-                </div>
-                <div class="suggestion-link">
-                    <a href="/meklet/rezultati?q=Filorga">Filorga
-                        <img src="https://semexit.s3-eu-west-1.amazonaws.com/Azeta/long-arrow-up.svg" alt="arrow">
-                    </a>
-                </div>
-                <div class="suggestion-link">
-                    <a href="/meklet/rezultati?q=Bezkontakta termometri">Bezkontakta termometri
-                        <img src="https://semexit.s3-eu-west-1.amazonaws.com/Azeta/long-arrow-up.svg" alt="arrow">
-                    </a>
-                </div>
-            </div>
-        </div>`.trim();
+        trendingSearches.innerHTML =
+            '<div class="first-block"> <div class="block-title"><p>Meklēšanas vēsture</p></div> <div class="test-history-list"></div> </div> <div class="second-block"> <div class="block-title"><img src="https://semexit.s3-eu-west-1.amazonaws.com/Azeta/star.svg" alt="star"><p>Aktuālie meklējumi</p></div> <div class="suggestions-list"> <div class="suggestion-link"> <a href="/meklet/rezultati?q=bioderma">Bioderma <img src="https://semexit.s3-eu-west-1.amazonaws.com/Azeta/long-arrow-up.svg" alt="arrow"> </a> </div> <div class="suggestion-link"> <a href="/meklet/rezultati?q=Vitamīns D">Vitamīns D <img src="https://semexit.s3-eu-west-1.amazonaws.com/Azeta/long-arrow-up.svg" alt="arrow"> </a> </div> <div class="suggestion-link"> <a href="/meklet/rezultati?q=Magnijs">Magnijs <img src="https://semexit.s3-eu-west-1.amazonaws.com/Azeta/long-arrow-up.svg" alt="arrow"> </a> </div> <div class="suggestion-link"> <a href="/meklet/rezultati?q=Eucerin">Eucerin <img src="https://semexit.s3-eu-west-1.amazonaws.com/Azeta/long-arrow-up.svg" alt="arrow"> </a> </div> <div class="suggestion-link"> <a href="/meklet/rezultati?q=Filorga">Filorga <img src="https://semexit.s3-eu-west-1.amazonaws.com/Azeta/long-arrow-up.svg" alt="arrow"> </a> </div> <div class="suggestion-link"> <a href="/meklet/rezultati?q=Bezkontakta termometri">Bezkontakta termometri <img src="https://semexit.s3-eu-west-1.amazonaws.com/Azeta/long-arrow-up.svg" alt="arrow"> </a> </div> </div> </div>';
         block.append(trendingSearches);
 
         var history = document.querySelectorAll('.sn-suggest-history-item');
