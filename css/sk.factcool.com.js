c853cf92ebb5f27c118961f8a39087908d723bd5
Giedrius <lauruska.giedrius@gmail.com>
2021-05-13 10:06:44 +0300

    save topo centras

--- css/sk.factcool.com.js
+++ css/sk.factcool.com.js
@@ -1,64 +1,54 @@
-function insertBefore(element, firstChild) {
-    firstChild.parentNode.insertBefore(element, firstChild);
-}
+// var newCta = document.createElement('div');
+// newCta.setAttribute('class', 'new-cta');
 
-function insertAfter(el, referenceNode) {
-    referenceNode.parentNode.insertBefore(el, referenceNode.nextSibling);
-}
+// var body = document.querySelector('body');
+// var prices = document.querySelector(
+//     'body > div.site-wrapper > section > div.product-detail-wrapper.marginb48 > div > div.col-xs-100.col-sm-50.col-lg-38.product-info > div.pd-prices'
+// );
+// var CTA = document.querySelector('div:nth-child(3) > div > div.col-xs-80.col-md-70.col-lg-65.pd-add-to-cart > button')
+//     ? document.querySelector('div:nth-child(3) > div > div.col-xs-80.col-md-70.col-lg-65.pd-add-to-cart > button')
+//     : document.querySelector('div:nth-child(4) > div > div.col-xs-80.col-md-70.col-lg-65.pd-add-to-cart > button');
 
-setTimeout(() => {
-    var newCta = document.createElement('div');
-    newCta.setAttribute('class', 'new-cta');
+// newCta.append(prices.cloneNode(true));
+// newCta.append(CTA.cloneNode(true));
 
-    var body = document.querySelector('body');
-    var prices = document.querySelector(
-        'body > div.site-wrapper > section > div.product-detail-wrapper.marginb48 > div > div.col-xs-100.col-sm-50.col-lg-38.product-info > div.pd-prices'
-    );
-    var CTA = document.querySelector('div:nth-child(3) > div > div.col-xs-80.col-md-70.col-lg-65.pd-add-to-cart > button')
-        ? document.querySelector('div:nth-child(3) > div > div.col-xs-80.col-md-70.col-lg-65.pd-add-to-cart > button')
-        : document.querySelector('div:nth-child(4) > div > div.col-xs-80.col-md-70.col-lg-65.pd-add-to-cart > button');
+// body.append(newCta);
 
-    newCta.append(prices.cloneNode(true));
-    newCta.append(CTA.cloneNode(true));
+// var newCtaButton = document.querySelector('body > div.new-cta > button');
+// newCtaButton.addEventListener('click', function () {
+//     CTA.click();
+// });
+// var productImage = document.querySelector('#slick-slide10 > div > img');
+// var productTitle = document.querySelector('.pd-mobile-title').cloneNode(true);
 
-    body.append(newCta);
+// var modalInterval = window.setInterval(function () {
+//     var modal = document.querySelector('body > div.sweet-alert.showSweetAlert.visible');
 
-    var newCtaButton = document.querySelector('body > div.new-cta > button');
-    newCtaButton.addEventListener('click', function () {
-        CTA.click();
-    });
-    var productImage = document.querySelector('#slick-slide10 > div > img');
-    var productTitle = document.querySelector('.pd-mobile-title').cloneNode(true);
+//     if (modal) {
+//         var modalText = document.querySelector('body > div.sweet-alert.showSweetAlert.visible > h2');
+//         var modalSizes = document.querySelector('body > div.sweet-alert.showSweetAlert.visible > p');
+//         var modalContent = document.createElement('div');
 
-    var modalInterval = window.setInterval(() => {
-        var modal = document.querySelector('body > div.sweet-alert.showSweetAlert.visible');
+//         modalContent.setAttribute('class', 'new-modal-content');
+//         modalContent.innerHTML = '<div class="image-side"></div><div class="content-side"></div>';
 
-        if (modal) {
-            var modalText = document.querySelector('body > div.sweet-alert.showSweetAlert.visible > h2');
-            var modalSizes = document.querySelector('body > div.sweet-alert.showSweetAlert.visible > p');
-            var modalContent = document.createElement('div');
+//         modal.append(modalContent);
 
-            modalContent.setAttribute('class', 'new-modal-content');
-            modalContent.innerHTML = '<div class="image-side"></div><div class="content-side"></div>';
+//         var newImage = document.createElement('img');
+//         newImage.setAttribute('src', productImage.src);
 
-            modal.append(modalContent);
+//         document.querySelector('.image-side').append(newImage);
+//         document.querySelector('.content-side').append(productTitle);
+//         document.querySelector('.content-side').append(modalText);
+//         document.querySelector('.content-side').append(modalSizes);
 
-            var newImage = document.createElement('img');
-            newImage.setAttribute('src', productImage.src);
+//         clearInterval(modalInterval);
+//     }
+// }, 30);
 
-            document.querySelector('.image-side').append(newImage);
-            document.querySelector('.content-side').append(productTitle);
-            document.querySelector('.content-side').append(modalText);
-            document.querySelector('.content-side').append(modalSizes);
-
-            clearInterval(modalInterval);
-        }
-    }, 30);
-
-    setInterval(() => {
-        var modalCta = document.querySelector('body > div.sweet-alert.showSweetAlert.visible > div.sa-button-container > div > button');
-        if (modalCta) {
-            modalCta.innerHTML = CTA.innerHTML;
-        }
-    }, 300);
-}, 1000);
+// setInterval(function () {
+//     var modalCta = document.querySelector('body > div.sweet-alert.showSweetAlert.visible > div.sa-button-container > div > button');
+//     if (modalCta) {
+//         modalCta.innerHTML = CTA.innerHTML;
+//     }
+// }, 300);
--- css/topocentras.lt.js
+++ css/topocentras.lt.js
@@ -1,87 +1,51 @@
-// function insertAfter(el, referenceNode) {
-//     referenceNode.parentNode.insertBefore(el, referenceNode.nextSibling);
-// }
+function insertBefore(element, destination) {
+    destination.parentNode.insertBefore(element, destination);
+}
 
-// function insertBefore(element, container, firstChild) {
-//     container.insertBefore(element, firstChild);
-// }
+setTimeout(() => {
+    var prisijungti =
+        document.querySelector('div.TopBar-topBarContainer-1E0 > button') ??
+        document.querySelector('div.TopBar-topBarContainer-1E0 > div.TopBar-buttonContainer-3dd > a.TopBar-button-3MC');
+    var krepselis = document.querySelector('div.Cart-cartContainer-1zC > div');
 
-// var productInterval = window.setInterval(function () {
-//     var searchIcon = document.querySelector('.HeaderContent-searchButton-1mR');
-//     var searchInput = document.querySelector('.HeaderContent-searchInput-3Ks');
-//     var akcijosLink = document.querySelector('.DesktopMenu-firstLevelMenuItem-3ve.firstLevelMenu-1 > a');
+    insertBefore(prisijungti.cloneNode(true), krepselis);
 
-//     if (searchIcon && akcijosLink) {
-//         var profileLink = document.querySelector(
-//             '#root > header.header-desktopHeader-1gy.show-from-desktop > div.header-topBarWrapper-2bd > div.TopBar-topBarContainer-1E0 > div.TopBar-buttonContainer-3dd > a'
-//         );
-//         var container = document.querySelector('.TopBar-buttonContainer-3dd');
-//         var tracking = document.querySelector('.HeaderContent-orderTracking-3gI');
-//         var paslaugosLink = document.querySelector('.DesktopMenu-firstLevelMenuItem-3ve.firstLevelMenu-2 > a');
-//         var appleLi = document.querySelector('.DesktopMenu-firstLevelMenuItem-3ve.firstLevelMenu-2');
-//         var prekiniaiZenklai = document.querySelector('.DesktopMenu-firstLevelMenuItem-3ve.firstLevelMenu-4');
-//         var kaipPirkti = document.querySelector('.DesktopMenu-firstLevelMenuItem-3ve.firstLevelMenu-6');
-//         var parduotuvesAtidarytos = document.querySelector('.DesktopMenu-firstLevelMenuItem-3ve.firstLevelMenu-7');
-//         var isparduotuve = document.querySelector('.DesktopMenu-firstLevelMenuItem-3ve.firstLevelMenu-5 > a');
-//         var delivery = document.querySelector('.TopBar-delivery-3wI');
-//         var wallet = document.querySelector('.TopBar-wallet-_8S');
-//         var returns = document.querySelector('.TopBar-return-1Mz');
-//         var freeDelivery = document.querySelector('.TopBar-free-3Ed');
-//         var atsijungti = document.querySelector(
-//             '#root > header.header-desktopHeader-1gy.show-from-desktop > div.header-topBarWrapper-2bd > div.TopBar-topBarContainer-1E0 > div.TopBar-buttonContainer-3dd > button'
-//         );
+    var newPridijungti = document.querySelector('div.HeaderContent-headerContentContainer-2N9 > div.Cart-cartContainer-1zC > button');
 
-//         searchIcon.textContent = '';
-//         searchInput.placeholder = 'Ieškokite prekės, kategorijos...';
-//         akcijosLink.textContent = 'Akcijos ir pasiūlymai';
-//         paslaugosLink.textContent = 'Teikiame net 40 paslaugų';
+    newPridijungti.addEventListener('click', function () {
+        prisijungti.click();
+    });
 
-//         //Removing items
-//         if (prekiniaiZenklai) {
-//             prekiniaiZenklai.remove();
-//         }
-//         if (kaipPirkti) {
-//             kaipPirkti.remove();
-//         }
-//         if (parduotuvesAtidarytos) {
-//             parduotuvesAtidarytos.remove();
-//         }
-//         if (delivery) {
-//             delivery.remove();
-//         }
-//         if (wallet) {
-//             wallet.remove();
-//         }
-//         if (returns) {
-//             returns.remove();
-//         }
+    var header = document.querySelector('#root > header.header-desktopHeader-1gy.show-from-desktop');
+    var button = document.createElement('div');
+    button.setAttribute('class', 'new-meniu-button');
+    header.append(button);
+    var addedButton = document.querySelector('.new-meniu-button');
 
-//         if (profileLink) {
-//             insertBefore(tracking, container, profileLink);
-//         } else {
-//             insertBefore(tracking, document.querySelector('.TopBar-topBarContainer-1E0'), document.querySelector('.TopBar-loginButton-1Xm'));
-//         }
+    addedButton.addEventListener('click', function () {
+        var meniuList = document.querySelector('#desktopMenu');
+        var maskRoot = document.querySelector('.mask-root-1oJ');
 
-//         var mieleParduotuve = document.createElement('li');
-//         mieleParduotuve.setAttribute('class', 'DesktopMenu-firstLevelMenuItem-3ve firstLevelMenu-3');
-//         mieleParduotuve.innerHTML = '<a class="DesktopMenu-link-4C-" href="/miele?ref=meniu">Miele parduotuvė</a>';
+        if (maskRoot.classList.contains('mask-root_active-3D9') === false) {
+            maskRoot.classList.add('mask-root_active-3D9');
+        } else {
+            maskRoot.classList.remove('mask-root_active-3D9');
+        }
 
-//         var darboLaikas = document.createElement('div');
-//         darboLaikas.setAttribute('class', 'darbo-laikas');
-//         darboLaikas.innerHTML = '<span>I - V 08:00 - 20:00</span><span>VI - VII 09:00 -17:00</span>';
+        if (meniuList.classList.contains('displayed-meniu') === false) {
+            meniuList.classList.add('displayed-meniu');
+        } else {
+            meniuList.classList.remove('displayed-meniu');
+        }
+    });
 
-//         freeDelivery.textContent = '+370 660 00200';
-//         freeDelivery.href = '';
+    window.onscroll = function () {
+        var currentScrollPos = window.pageYOffset;
 
-//         if (atsijungti) {
-//             atsijungti.innerHTML = '<img src="" />';
-//         }
-
-//         insertAfter(mieleParduotuve, appleLi);
-//         insertAfter(darboLaikas, freeDelivery);
-
-//         isparduotuve.innerHTML = '✂ ŠNAI PER KAINAS!';
-
-//         clearInterval(productInterval);
-//     }
-// }, 500);
+        if (currentScrollPos > 40) {
+            header.classList.add('scrolled');
+        } else {
+            header.classList.remove('scrolled');
+        }
+    };
+}, 1000);
